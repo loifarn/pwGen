c3cc6674600df7f3e44357bc837b1add6fb8003b
Alex Pedersen <alex@loif.no>
2017-02-18 14:42:25 +0100

    pwGen - Started GUI Design

---
+++ src/pwGui.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+public class pwGui {
+
+    private JTextPane pwDisplay;
+    private JPanel panel1;
+    private JCheckBox pwNumCheck;
+    private JCheckBox pwScharCheck;
+    private JButton pwGenButton;
+    private JTextField pwLengthField;
+    private JLabel pwNumLabel;
+    private JLabel pwsScharLabel;
+
+    public pwGui() {
+
+    }
+}
